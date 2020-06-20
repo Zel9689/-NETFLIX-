bb159a9216468cdf98c839750d29ebaa21d27ae1
zel9689 <57132523+Zel9689@users.noreply.github.com>
2020-06-21 04:32:38 +0800

    算NETFLIX家庭版 一個人在指定日期間要

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//每月2號刷卡，看那天的匯率*當月的美金
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ConsoleApp1;
+
+namespace NETFLIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal beforePay = 13.99M;
+            decimal afterPay = 15.99M;
+            int[] getting_expensive = new int[2] {2019, 2};
+            DateTime localDate = DateTime.Now;
+            Console.Write("輸入從何時開始寄生串流的(YYYY-MM): ");
+            string input = Console.ReadLine();
+            List<int> joinTime = new List<int> ();
+            foreach(string str in input.Split("-"))
+                joinTime.Add(Convert.ToInt32(str));
+            string url = "https://www.esunbank.com.tw/bank/personal/deposit/rate/forex/exchange-rate-chart?Currency=USD/TWD";
+            webControl x = new webControl(url, joinTime);
+            decimal totalPay = 0M;
+            decimal totalPayInUSD = 0M;
+            decimal pay;
+            while(true)
+            {
+                if(x.date[0] == localDate.Year && x.date[1] > localDate.Month)
+                    break;
+                x.SearchDate();
+                decimal rate = x.GetThisMonthRate();
+                if(x.date[0] < getting_expensive[0] || 
+                    (x.date[0] == getting_expensive[0] && x.date[1] < getting_expensive[1]))
+                    pay = beforePay / 4;
+                else
+                    pay = afterPay / 4;
+                if(x.date[1] < 12)
+                    x.date[1] += 1;
+                else
+                {
+                    x.date[0] += 1;
+                    x.date[1] = 1;
+                }
+                totalPayInUSD += pay;
+                totalPay += rate * pay;
+            }
+            x.Quit();
+            Console.WriteLine($"總共{totalPayInUSD}美金，{totalPay}台幣");
+            Console.Write("按任意鍵結束");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ webControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp1
+{
+    public class webControl
+    {
+        IWebDriver driver = new ChromeDriver();
+        public string url{ get; set; }
+        public List<int> date{ get; set; }
+
+        public webControl(string url, List<int> list)
+        {
+            this.url = url;
+            this.date = list;
+            this.Init();
+        }
+        public void Init()
+        {
+            //開啟網頁
+            driver.Navigate().GoToUrl(url);
+            //隱式等待 - 直到畫面跑出資料才往下執行
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10000);
+        }
+        public void SearchDate()
+        {
+            //找起始日期輸入框
+            IWebElement StartDate = driver.FindElement(By.XPath("/html/body/form/div[7]/div[3]/div[4]/div/div[1]/div[2]/div[2]/input[1]"));
+            Thread.Sleep(400);
+            StartDate.Clear();
+            Thread.Sleep(400);
+            StartDate.SendKeys($"{this.date[0]}-{this.date[1]}-02\n");
+            Thread.Sleep(400);
+            //點數據表
+            IWebElement RadioButton = driver.FindElement(By.XPath("html/body/form/div[7]/div[3]/div[4]/div/div[1]/div[3]/div/img[2]"));
+            Thread.Sleep(400);
+            RadioButton.Click();
+            Thread.Sleep(400);
+            //搜尋
+            IWebElement calendarButton = driver.FindElement(By.XPath("/html/body/form/div[7]/div[3]/div[4]/div/div[1]/div[4]/a"));
+            Thread.Sleep(400);
+            calendarButton.Click();
+            Thread.Sleep(400);
+        }
+        public decimal GetThisMonthRate()
+        {
+            ReadOnlyCollection<IWebElement> rateList = driver.FindElements(By.ClassName("lastTd"));
+            Thread.Sleep(400);
+            decimal rate = Convert.ToDecimal(rateList[1].Text);
+            return rate;
+        }
+        public void Quit()
+        {
+            driver.Quit();
+        }
+    }
+}
